dca4149d4876249e748509f5896830075f340b0c
Feliverse <114632980+Feliverse@users.noreply.github.com>
2025-02-05 07:19:57 -0400

    String Length task 1 + test

--- stringLength.js
+++ stringLength.js
@@ -1,4 +1,10 @@
 function stringLength(string) {
+  if (string.length < 1) {
+    throw new Error('String length must be at least 1 character long.');
+  }
+  if (string.length > 10) {
+    throw new Error('String must not be longer than 10 characters.');
+  }
   return string.length;
 }
 
