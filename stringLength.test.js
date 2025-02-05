3c9832c2515ee8e3dd4b36ee58c33b6e61ffc60e
Feliverse <114632980+Feliverse@users.noreply.github.com>
2025-02-05 07:20:34 -0400

    Reverse String, Task 2, + test

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,5 +1,13 @@
 const stringLength = require('./stringLength');
 
 test('Returns the correct length of a string', () => {
-  expect(stringLength('Hello, World!')).toBe(13);
+  expect(stringLength('Hello')).toBe(5);
 });
+
+test('throws an error if the string is too short', () => {
+  expect(() => stringLength('')).toThrow('String length must be at least 1 character long.');
+})
+
+test('throws an error if the string is too long', () => {
+  expect(() => stringLength('this is too long')).toThrow('String must not be longer than 10 characters.');
+})
